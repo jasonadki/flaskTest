0233b0d03e37fb4087eda38e088a2cdf8e1aee86
Jason Adkin <adkija@safeautonet.net>
2018-05-05 22:00:43 -0400

    second test

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,22 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-	return 'Hello, World!'
+    user = {'username': 'Jason'}
+    posts = [
+    	{
+    		'author' : {'username': 'Josh'},
+    		'body' : 'Hey I"m Josh'
+    	},
+    	{
+    		'author' : {'username': 'Chris'},
+    		'body' : 'Hi I"m Christian'
+    	},
+    	{
+    		'author' : {'username': 'Josh'},
+    		'body' : 'Hey Chris whats up'
+    	}
+    ]
+    return render_template('index.html', title = 'Home', user = user, posts	= posts)
